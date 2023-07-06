4e43e2b93686cb5e53179feb7e87960c97774853
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-07-06 12:18:19 +0100

    type annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+""" a python script to find sum of a list"""
+
+
+def sum_list(input_list: List) -> float:
+    """function to return sum of a list"""
+    return sum(input_list)
